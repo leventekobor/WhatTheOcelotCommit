42cb2599de8839c1bce7ca0f73899df8f72f1d3f
Levente <levente.kobor94@gmail.com>
2018-07-04 08:47:35 +0200

    chore(WhatTheOcelotCommit)

---
+++ WhatTheCommitStealing/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WhatTheCommitStealing.Models;
+using WhatTheCommitStealing.Repository;
+
+namespace WhatTheCommitStealing.Controllers
+{
+    public class HomeController : Controller
+    {
+        private Dwa dwa = new Dwa();
+
+        public IActionResult Index()
+        {
+            return View(dwa);
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Why you here? Do not you have something to commit?";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Please do not contact us, We do not know either";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WhatTheCommitStealing/Repository/dwa.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WhatTheCommitStealing.Repository
+{
+    public class Dwa
+    {
+        public string GetStuff()
+        {
+            Random random = new Random();
+            string[] commits = File.ReadAllLines(@"funny.txt");
+            return commits[random.Next(commits.Length)];
+        }
+
+    }
+}
